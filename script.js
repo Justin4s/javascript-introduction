80cb20659acbc8aaf8896f10695492714c82fba5
Justin4s <105396961+Justin4s@users.noreply.github.com>
2022-06-23 02:16:13 +0100

    fourth commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const aboutme = {
+    name: 'Nyowheoma Ejovwokoghene Justin',
+    height: '5.4 inches',
+    country: 'Nigeria',
+};
+
+alert(
+    `My name is ${aboutme.name} , I am ${aboutme.height} tall and I am from ${aboutme.country}.`
+);
